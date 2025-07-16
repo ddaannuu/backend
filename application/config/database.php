fc5e60b068d11e53cef11396471b4cd65ee7974f
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-07-16 08:05:04 +0700

    da

--- application/config/database.php
+++ application/config/database.php
@@ -73,28 +73,54 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'mysql-technologia.mysql.database.azure.com',
+// 	'username' => 'admintechnologia',
+// 	'password' => '',
+// 	'database' => 'db_technologia',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE,
+// 	'encrypt' => TRUE, // ← tambahkan ini
+// 	'ssl_verify' => FALSE, // opsional, TRUE kalau kamu pakai sertifikat
+// );
+
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'mysql-technologia.mysql.database.azure.com',
-	'username' => 'admintechnologia',
-	'password' => '4kUc4hb4g03S',
-	'database' => 'db_technologia',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE,
-	'encrypt' => TRUE, // ← tambahkan ini
-	'ssl_verify' => FALSE, // opsional, TRUE kalau kamu pakai sertifikat
+    'dsn'	=> '',
+    'hostname' => 'mysql-technologia.mysql.database.azure.com',
+    'username' => 'admintechnologia@mysql-technologia',
+    'password' => '4kUc4hb4g03S',
+    'database' => 'db_technologia',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => TRUE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE,
+    'ssl_options' => array(
+        'ssl_ca' => '/site/certs/BaltimoreCyberTrustRoot.crt.pem'  // default path di Azure App Service
+    ),
 );
+
 
 
