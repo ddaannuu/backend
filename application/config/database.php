2b0aadaf18ae87abeada5ed19f03449043133871
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-07-16 08:17:21 +0700

    da

--- application/config/database.php
+++ application/config/database.php
@@ -124,7 +124,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 'hostname' => 'mysql-technologia.mysql.database.azure.com',
-'username' => 'admintechnologia@mysql-technologia',
+'username' => 'admintechnologia',
 'password' => '4kUc4hb4g03S',
 'database' => 'db_technologia',
 'dbdriver' => 'mysqli',
