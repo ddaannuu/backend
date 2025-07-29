032629e6d2acef54ff99aa1fef2ce9a49b6bffc2
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-07-29 19:49:37 +0700

    d

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -176,28 +176,31 @@ class Users extends CI_Controller {
 
 
 	public function list_api() {
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        header("Access-Control-Allow-Origin: https://nice-flower-0c59cd800.1.azurestaticapps.net");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-        header("Access-Control-Allow-Headers: Content-Type, Authorization");
-        header("Access-Control-Allow-Credentials: true");
-        http_response_code(200);
-        exit;
-    }
+    // Tangani preflight CORS
+		if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+			header("Access-Control-Allow-Origin: https://nice-flower-0c59cd800.1.azurestaticapps.net");
+			header("Access-Control-Allow-Credentials: true");
+			header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+			header("Access-Control-Allow-Headers: Content-Type, Authorization");
+			http_response_code(200);
+			exit;
+		}
 
-    header("Access-Control-Allow-Origin: https://nice-flower-0c59cd800.1.azurestaticapps.net");
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    header('Content-Type: application/json');
+		// Header CORS utama
+		header("Access-Control-Allow-Origin: https://nice-flower-0c59cd800.1.azurestaticapps.net");
+		header("Access-Control-Allow-Credentials: true");
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+		header("Access-Control-Allow-Headers: Content-Type, Authorization");
+		header('Content-Type: application/json');
 
-    $users = $this->User_model->get_all_users();
+		$users = $this->User_model->get_all_users();
 
-    echo json_encode([
-        'status' => true,
-        'data' => $users
-    ]);
-}
+		echo json_encode([
+			'status' => true,
+			'data' => $users
+		]);
+	}
+
 
 
 
