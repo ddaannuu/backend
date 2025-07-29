e68e10563d32e1df4ea55d4b2de732889fd008f7
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-07-29 20:34:25 +0700

    d

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -175,37 +175,21 @@ class Users extends CI_Controller {
 
 
 
-	public function list_api() {
-    // Tangani preflight CORS
-	// WAJIB tambahkan di awal function list_api
-		header("Access-Control-Allow-Origin: https://nice-flower-0c59cd800.1.azurestaticapps.net");
-		header("Access-Control-Allow-Credentials: true");
-		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-		header("Access-Control-Allow-Headers: Content-Type, Authorization");
+	public function list_api()
+{
+    header("Access-Control-Allow-Origin: https://nice-flower-0c59cd800.1.azurestaticapps.net");
+    header("Access-Control-Allow-Credentials: true");
+    header("Content-Type: application/json");
 
-		if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-			header("Access-Control-Allow-Origin: https://nice-flower-0c59cd800.1.azurestaticapps.net");
-			header("Access-Control-Allow-Credentials: true");
-			header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-			header("Access-Control-Allow-Headers: Content-Type, Authorization");
-			http_response_code(200);
-			exit;
-		}
+    $this->load->model('User_model');
+    $users = $this->User_model->get_all();
 
-		// Header CORS utama
-		header("Access-Control-Allow-Origin: https://nice-flower-0c59cd800.1.azurestaticapps.net");
-		header("Access-Control-Allow-Credentials: true");
-		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-		header("Access-Control-Allow-Headers: Content-Type, Authorization");
-		header('Content-Type: application/json');
+    echo json_encode([
+        'status' => true,
+        'data' => $users
+    ]);
+}
 
-		$users = $this->User_model->get_all_users();
-
-		echo json_encode([
-			'status' => true,
-			'data' => $users
-		]);
-	}
 
 
 
