5ecdc7ddb4ea282e091daf2af038be7568eda1ce
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-07-29 20:47:01 +0700

    d

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Origin: https://nice-flower-0c59cd800.1.azurestaticapps.net");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    header("Access-Control-Allow-Credentials: true");
-    http_response_code(200);
-    exit;
-}
 
 /**
  * CodeIgniter
