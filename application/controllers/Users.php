4b508638b05f8e518c53fe727ea581f53321848c
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-07-30 19:58:06 +0700

    udah makan apa belum

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -175,11 +175,20 @@ class Users extends CI_Controller {
 
 
 
-	public function list_api() {
+public function list_api() {
+    // Tangani preflight dulu
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header("Access-Control-Allow-Origin: https://nice-flower-0c59cd800.1.azurestaticapps.net");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        header("Access-Control-Allow-Credentials: true");
+        http_response_code(200);
+        exit;
+    }
+
+    // Respons utama
     header("Access-Control-Allow-Origin: https://nice-flower-0c59cd800.1.azurestaticapps.net");
     header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header("Content-Type: application/json");
 
     $users = $this->User_model->get_all_users();
@@ -189,6 +198,7 @@ class Users extends CI_Controller {
         'data' => $users
     ]);
 }
+
 
 
 
