d38f4a6a03e6375a2ec3cf80604e598051de1d06
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-07-31 09:38:34 +0700

    putu

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -78,9 +78,9 @@ public function create_form() {
         $category    = $this->input->post('category');
         $buy_link    = $this->input->post('buy_link');
         $description = $this->input->post('description');
-		log_message('error', 'UPLOAD DEBUG: ' . dirname(APPPATH) . '/../vue-project/public/Images/');
+		// log_message('error', 'UPLOAD DEBUG: ' . dirname(APPPATH) . '/../vue-project/public/Images/');
 
-		$upload_dir = realpath(dirname(FCPATH) . 'vue-project\public\Images') . '/';
+		$upload_dir = FCPATH . 'uploads/';
 
 
 
